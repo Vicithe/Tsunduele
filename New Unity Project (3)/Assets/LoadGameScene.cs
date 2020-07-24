2a4384bc340a252951f8f5de057f4c0e8c2c1b0f
Emily Teng <emilyteng55@gmail.com>
2020-07-23 18:13:15 -0700

    Created GameScene and some preliminary transition scripts for LoadingScreen > GameScene and GameScene > endings.

---
+++ New Unity Project (3)/Assets/LoadGameScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //If statement is placeholder for now.
+        //Will determine once the Load screen is created what the trigger is to load the main game screen
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
