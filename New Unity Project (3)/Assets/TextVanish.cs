024d64ab44a40edb6075f02d72285211ff44fde2
Emily Teng <emilyteng55@gmail.com>
2020-07-28 20:12:45 -0700

    Added scene change from menu to main game scene. Adjusted dialogue int values. Got rid of instructions text box on scene change.

---
+++ New Unity Project (3)/Assets/TextVanish.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextVanish : MonoBehaviour
+{
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "GameScene")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
